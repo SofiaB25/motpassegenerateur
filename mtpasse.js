757e4cbd5dc487d25d5f1ec49ae2389e9f3d9892
SofiaB25 <sofia.bendib592@gmail.com>
2025-04-25 16:25:35 +0200

    travail pas fini

---
+++ mtpasse.js
@@ -0,0 +1,16 @@
+var copyBtn  = document.getElementBy('copy')
+
+function getPassword(length){
+   const majuscules = "ABCEFGHIJKLMNOPQRSTUVWXYZ";
+   const minuscules = "abcdefghijklmnopqrstuvwxyz";
+   const chiffres = "123456789";
+   const symboles = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+}
+for (let i = 0; i <passwordLength ; i++){
+     let ramdomNumber = Math.floor(Math.random * chars.length)
+    }
+    /const length = parseInt(document.getElementById('passwordLength').value, 10);
+    if (length < 12 || length > 128) {
+     alert('La longueur doit être comprise entre 12 et 128 caractères.');
+    return;
+}
